26efc360581acf19a84563b34a1c20573515600b
Nima <nima1981@gmail.com>
2023-12-24 17:06:13 -0500

    build errors

--- src/pages/api/inscriptions-content.tsx
+++ src/pages/api/inscriptions-content.tsx
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			let extension = 'txt';
 			
 			if (mimetype != undefined)
-			  extension = mimetype?.split('/')?.pop().replace('plain','txt');
+			  extension = mimetype?.split('/').pop()?.replace('plain','txt');
 			
 			if (download)
 		    {
