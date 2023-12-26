a0a02ce8edc5898ef9c5ad7be705d3768e695c6a
Nima <nima1981@gmail.com>
2023-12-26 17:49:07 -0500

    build errors

--- src/pages/api/inscriptions.tsx
+++ src/pages/api/inscriptions.tsx
@@ -46,7 +46,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	Object.keys(remainingQueryParams).map((parameterName, i) => {
     
 	  if (queryString != undefined && typeof queryString === 'string' && parameterName != undefined && typeof parameterName === 'string' && remainingQueryParams[parameterName] != undefined && typeof remainingQueryParams[parameterName] === 'string')
-	    queryString += queryString ? '&' + (parameterName as string) + "=" + encodeURIComponent(remainingQueryParams.parameterName as string) : parameterName + "=" + encodeURIComponent(remainingQueryParams.parameterName as string);
+	    queryString += queryString ? '&' + (parameterName as string) + "=" + encodeURIComponent(remainingQueryParams.parameterName as string) : (parameterName as string) + "=" + encodeURIComponent(remainingQueryParams.parameterName as string);
 	
 	  cacheFileName += cacheFileName ? '-' + parameterName + "-" + encodeURIComponent(remainingQueryParams[parameterName]) : parameterName + "-" + encodeURIComponent(remainingQueryParams[parameterName]);
 
